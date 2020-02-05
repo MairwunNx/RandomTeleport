7d48c1cd2cc505c474e7eeb27d1e0541a6a0dfb2
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-02-05 20:08:55 +0300

    In-commands settings removed.

--- src/main/kotlin/com/mairwunnx/randomteleport/configuration/ConfigurationModel.kt
+++ src/main/kotlin/com/mairwunnx/randomteleport/configuration/ConfigurationModel.kt
@@ -7,13 +7,9 @@ data class ConfigurationModel(
     var canTeleportOnTrees: Boolean = true,
     var defaultRadius: Int = 4096,
     var defaultAttempts: Int = 1,
-    var enableInCommandSettings: Boolean = false,
     var teleportStrategy: TeleportStrategy = TeleportStrategy.KEEP_LOADED,
     var teleportOnCenterBlock: Boolean = true,
     var interactWithEssentials: Boolean = true,
     var locationRollBackTimer: Int = 10,
-    var maxCommandTeleportAttempts: Int = 10,
-    var maxCommandTeleportRadius: Int = 10000,
-    var minCommandTeleportRadius: Int = 30,
     var minRandomTeleportRadius: Int = 30
 )
