56798640b8c8541edf3c5d415a80b85f6c27eff0
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-02-06 15:26:42 +0300

    Now as gradle settings uses kotlin gradle dsl script.

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        jcenter()
+        gradlePluginPortal()
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+    }
+
+    plugins {
+        id("fabric-loom") version extra.properties["loom_version"].toString()
+        kotlin("jvm") version extra.properties["kotlin_version"].toString()
+    }
+}
