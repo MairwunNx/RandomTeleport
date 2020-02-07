0805be4bc39897aed6fa09b98e0cbf0041209009
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-02-07 13:59:49 +0300

    EntryPoint.kt cleanup. Checking availability project essentials modules removed.

--- src/main/kotlin/com/mairwunnx/randomteleport/EntryPoint.kt
+++ src/main/kotlin/com/mairwunnx/randomteleport/EntryPoint.kt
@@ -1,6 +1,5 @@
 package com.mairwunnx.randomteleport
 
-import com.mairwunnx.projectessentials.permissions.permissions.PermissionsAPI
 import com.mairwunnx.randomteleport.commands.BadLocationCommand
 import com.mairwunnx.randomteleport.commands.RandomTeleportCommand
 import com.mairwunnx.randomteleport.managers.ConfigurationManager
@@ -20,26 +19,7 @@ class EntryPoint {
     init {
         logger.info("Random Teleport mod initializing")
         ConfigurationManager.load()
-        loadAdditionalModules()
         MinecraftForge.EVENT_BUS.register(this)
-    }
-
-    private fun loadAdditionalModules() {
-        if (ConfigurationManager.get().interactWithEssentials) {
-            try {
-                Class.forName(permissionAPIClassPath)
-                permissionsInstalled = true
-            } catch (_: ClassNotFoundException) {
-                // ignored
-            }
-
-            try {
-                Class.forName(cooldownAPIClassPath)
-                cooldownInstalled = true
-            } catch (_: ClassNotFoundException) {
-                // ignored
-            }
-        }
     }
 
     @SubscribeEvent
@@ -56,22 +36,9 @@ class EntryPoint {
     }
 
     companion object {
-        private const val permissionAPIClassPath =
-            "com.mairwunnx.projectessentials.permissions.permissions.PermissionsAPI"
-        private const val cooldownAPIClassPath =
-            "com.mairwunnx.projectessentials.cooldown.essentials.CooldownAPI"
-
-        private var permissionsInstalled: Boolean = false
-        var cooldownInstalled: Boolean = false
-
         internal fun hasPermission(
             player: ServerPlayerEntity,
-            node: String,
             opLevel: Int
-        ): Boolean = if (permissionsInstalled) {
-            PermissionsAPI.hasPermission(player.name.string, node)
-        } else {
-            player.server.opPermissionLevel >= opLevel
-        }
+        ): Boolean = player.server.opPermissionLevel >= opLevel
     }
 }
