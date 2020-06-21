59d2b7ee3fcdbf2e41f59234c5d5d2789456b915
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-21 21:17:17 +0300

    TeleportRollbackManager.kt re-written.

--- src/main/kotlin/com/mairwunnx/randomteleport/managers/TeleportRollbackManager.kt
+++ src/main/kotlin/com/mairwunnx/randomteleport/managers/TeleportRollbackManager.kt
@@ -1,6 +1,6 @@
 package com.mairwunnx.randomteleport.managers
 
-import com.google.common.collect.HashBasedTable
+import com.google.common.collect.HashBasedTable.create
 import com.mairwunnx.randomteleport.structs.Position
 import org.apache.logging.log4j.LogManager
 import java.time.Duration
@@ -9,31 +9,19 @@ import java.time.ZonedDateTime.now
 
 object TeleportRollbackManager {
     private val logger = LogManager.getLogger()
-    private val lastPosition =
-        HashBasedTable.create<String, Position, ZonedDateTime>()
+    private val lastPosition = create<String, Position, ZonedDateTime>()
 
-    /**
-     * @param playerName player nickname.
-     * @param position last player position
-     * (before teleporting to random location).
-     */
-    fun commitPosition(playerName: String, position: Position) {
-        logger.debug("Position $position committing for $playerName")
-        purgeAll().also { removeEntry(playerName) }
-        lastPosition.put(playerName, position, now())
+    fun commitPosition(playerName: String, position: Position) =
+        logger.debug("Position $position committing for $playerName").also {
+            purgeAll().also { removeEntry(playerName) }.run {
+                lastPosition.put(playerName, position, now())
+            }
+        }
+
+    fun removeEntry(playerName: String) = lastPosition.containsRow(playerName).let {
+        if (it) lastPosition.rowMap().remove(playerName)
     }
 
-    fun removeEntry(playerName: String) {
-        if (lastPosition.containsRow(playerName)) lastPosition.rowMap().remove(playerName)
-    }
-
-    /**
-     * @param playerName player nickname.
-     * @return `Position` instance if possible
-     * rollback player location, or timer not
-     * expired. `Null` if timer expired or
-     * not exist last position for player.
-     */
     fun requestPosition(playerName: String): Position? {
         val pos by lazy { lastPosition.rowMap().getValue(playerName).keys.first() }
         logger.debug("Requesting old position for $playerName").also { purgeAll() }
@@ -44,9 +32,6 @@ object TeleportRollbackManager {
         }
     }
 
-    /**
-     * Removes all expired timers for position rollback.
-     */
     private fun purgeAll() {
         logger.debug("Purging all expired location rollback entries")
         lastPosition.rowMap().keys.removeAll {
