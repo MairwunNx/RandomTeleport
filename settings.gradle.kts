a3da2331e88564205db4b11c60f64d58aa03c8cf
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-21 21:08:53 +0300

    settings.gradle.kts re-written.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,11 @@
 pluginManagement {
     repositories {
-        jcenter()
-        gradlePluginPortal()
+        jcenter().apply { gradlePluginPortal() }
         maven {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
     }
-
     plugins {
         id("fabric-loom") version extra.properties["loom_version"].toString()
         kotlin("jvm") version extra.properties["kotlin_version"].toString()
